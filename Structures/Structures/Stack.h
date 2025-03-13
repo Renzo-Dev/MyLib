ca8f6409ad880316f390ded90ede3e612c032ca9
Administrator <dankoshevoy2001@gmail.com>
2025-03-13 14:00:11 +0300

    fix

---
+++ Structures/Structures/Stack.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <stdexcept>
+
+template <typename T>
+class Stack
+{
+	struct Node
+	{
+		T data;
+		Node* next;
+
+		Node(T value) : data(value), next(nullptr)
+		{
+		}
+	};
+
+	Node* head;
+	int size;
+
+public:
+	Stack() : head(nullptr), size(0)
+	{
+	}
+
+	void push(T value)
+	{
+		Node* newNode = new Node(value);
+		newNode->next = head;
+		head = newNode;
+		size++;
+	}
+
+	T pop()
+	{
+		if (head == nullptr)
+		{
+			return 0;
+		}
+
+		Node* temp = head;
+		T value = head->data;
+		head = head->next;
+		delete temp;
+		size--;
+		return value;
+	}
+
+	T peek()
+	{
+		if (head) return head->data;
+		// throw std::runtime_error("Stack is empty.");
+		return 0;
+	}
+
+	// if empty return true, else return false
+	bool isEmpty()
+	{
+		return head == nullptr;
+	}
+
+	int length() const
+	{
+		return size;
+	}
+};
---
+++ Structures/Structures/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Stack.h"
+
+using namespace std;
+
+int main()
+{
+	Stack<int> s = Stack<int>();
+}
